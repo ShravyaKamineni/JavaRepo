ae8402ba58468f10012a73ca049d93146c9b437c
Naga Shravya <shravyay14cs861@gmail.com>
2020-03-10 11:15:35 -0500

    Added 2 new files

---
+++ Pdemo1.java
@@ -0,0 +1,24 @@
+class Pdemo1{
+byte b;
+short s;
+int i;
+long l;
+float f;
+double d;
+char ch;
+boolean flag;
+public static void main(String[] args)
+{
+Pdemo1 d1 = new Pdemo1();
+System.out.println(d1.b);
+System.out.println(d1.s);
+System.out.println(d1.i);
+System.out.println(d1.l);
+System.out.println(d1.f);
+System.out.println(d1.d);
+System.out.println(d1.ch);
+System.out.println(d1.flag);
+}
+}
+
+
---
+++ Stat.java
@@ -0,0 +1,9 @@
+public class Stat{
+int x;
+static int y;
+public static void main(String[] args){
+Stat s1= null;
+s1.y = s1.y+1;
+System.out.println(s1.y);
+}s
+}
